8f3279646981db462535f28c2d0c1ac266097809
Roberval Filho <rslfilho@gmail.com>
2022-02-02 20:36:13 -0300

    Atualizando CORS no socket

--- src/api/socket.js
+++ src/api/socket.js
@@ -8,7 +8,7 @@ const io = socket(
   httpServer,
   {
     cors: {
-      origin: 'https://g3-delivery.vercel.app/',
+      origin: 'https://g3-delivery.vercel.app',
       methods: ['GET', 'POST'],
     },
   },
