9b03536a66d7c12151be1f2b1504dbed887384ac
Roberval Filho <rslfilho@gmail.com>
2022-02-02 20:11:11 -0300

    Tirando sufixo

--- src/database/config/config.js
+++ src/database/config/config.js
@@ -14,7 +14,7 @@ const options = {
   host: process.env.MYSQL_HOST || 'mysql',
   port: process.env.MYSQL_PORT || '3306',
   database: 
-    `${process.env.MYSQL_DB_NAME || 'delivery-app'}${suffix[environment] || suffix.test}`,
+    `${process.env.MYSQL_DB_NAME || 'delivery-app'}`,
   username: process.env.MYSQL_USER || 'root',
   password: process.env.MYSQL_PASSWORD || 'password',
   dialect: 'mysql',
