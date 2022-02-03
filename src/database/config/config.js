80d7645e8b91aeb8c4978926f25cc6b27dfd7f46
Roberval Filho <rslfilho@gmail.com>
2022-02-03 08:33:33 -0300

    Atualizando seeders de produtos

--- src/database/config/config.js
+++ src/database/config/config.js
@@ -13,8 +13,7 @@ const suffix = {
 const options = {
   host: process.env.MYSQL_HOST || 'mysql',
   port: process.env.MYSQL_PORT || '3306',
-  database: 
-    `${process.env.MYSQL_DB_NAME || 'delivery-app'}`,
+  database: `${process.env.MYSQL_DB_NAME || 'delivery-app'}`,
   username: process.env.MYSQL_USER || 'root',
   password: process.env.MYSQL_PASSWORD || 'password',
   dialect: 'mysql',
